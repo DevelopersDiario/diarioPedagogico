df79dc69b1aaf0679651cf02169b4d9f63a176e9
dereckz <dereckz1304@gmail.com>
2018-02-12 22:17:59 -0600

    Nueva URL

---
+++ app/src/main/java/com/dese/diario/Utils/Session.java
@@ -0,0 +1,44 @@
+package com.dese.diario.Utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Eduardo on 05/02/2018.
+ */
+
+
+public class Session {
+
+    private final String SHARED_PREFS_FILE = "HMPrefs";
+    private final String KEY_EMAIL = "email";
+
+    private final String KEY_PASS = "pass";
+
+    private Context mContext;
+
+
+    public Session (Context context){
+        this.mContext=context;
+    }
+    private SharedPreferences getSettings(){
+        return mContext.getSharedPreferences(SHARED_PREFS_FILE, 0);
+    }
+
+    public String getUserEmail(){
+        return getSettings().getString(KEY_EMAIL, null);
+    }
+
+    public void setUserEmail(String email){
+        SharedPreferences.Editor editor = getSettings().edit();
+        editor.putString(KEY_EMAIL, email );
+        editor.commit();
+    }
+
+    public void setUserPass(String pass){
+        SharedPreferences.Editor editor = getSettings().edit();
+        editor.putString(KEY_PASS, pass );
+        editor.commit();
+    }
+
+}
--- app/src/main/java/com/dese/diario/Utils/Urls.java
+++ app/src/main/java/com/dese/diario/Utils/Urls.java
@@ -8,64 +8,64 @@ public class Urls {
 
 /** Server Online **/
     /***
+     **/
+
+     public static final String login = "http://201.144.197.52:8090/diariopws/api/1.0/user/autenticar";                        //en uso
+     public static final String createaccount = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/insert";                //en uso
+
+     //URLs Seleccionar usurio
+     public static final String userslist = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/list";           //Funcional en server
+     public static final String ufilXnombre = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/filtrousuarioXnombre"; //Funcional en server
+     public static final String filtrousuarioXid = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/filtrousuarioXid";  //Funcional en server
+     public static final String download = "http://201.144.197.52:8090/diariopws/api/1.0/archivos/descargar/";         //en uso  "local"
+     public static final String upload = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/upload/";
+     public static final String uploadholder = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/uploadfPortada";         //en uso  "local"
+
+     ////actualizacion de informacion de perfil
+     public static final String updateestado = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/updateestado";                //en uso
+     public static final String updateinfPersonal = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/updateinfPersonal";       //en uso
+     public static final String updateinfacademica = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/updateinfacademica";     //en uso
+     public static final String updateinfsocial = "http://201.144.197.52:8090/diariopws/api/1.0/usuario/updateinfsocial";           //en uso
 
 
-    public static final String login = "http://187.188.168.51:8090/diariopws/api/1.0/user/autenticar";                        //en uso
-    public static final String createaccount = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/insert";                //en uso
+     //URLs listar  publicaciones
+     //  public static final String listarpublicacionxiduser= "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/listxiduser";
+     public static final String listxgrupo = " http://201.144.197.52:8090/diariopws/api/1.0/publicacion/listpublicxgpo";
+     public static final String listpublicacion = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/list";                //en uso
+     public static final String listxiduser = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/listxiduser";  //funcional server "Requiere idusuario" como header
 
-    //URLs Seleccionar usurio
-    public static final String userslist = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/list";           //Funcional en server
-    public static final String ufilXnombre = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/filtrousuarioXnombre"; //Funcional en server
-    public static final String filtrousuarioXid = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/filtrousuarioXid";  //Funcional en server
-    public static final String download = "http://187.188.168.51:8090/diariopws/api/1.0/archivos/descargar/";         //en uso  "local"
-    public static final String upload = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/upload/";
-    public static final String uploadholder = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/uploadfPortada";         //en uso  "local"
+     //URLs insertar  publicaciones
+     public static final String insertpublicacion = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/createpublicacionpadre";   //En reconstruccion
+     public static final String repuplication = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/republication";
+     public static final String listarrepublication = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/listrepublication";
+     public static final String publicararchivo = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/publicarArchivo";
+     public static final String obtenerdetallepublicacion = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/listdetpublication";
 
-    ////actualizacion de informacion de perfil
-    public static final String updateestado = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/updateestado";                //en uso
-    public static final String updateinfPersonal = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/updateinfPersonal";       //en uso
-    public static final String updateinfacademica = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/updateinfacademica";     //en uso
-    public static final String updateinfsocial = "http://187.188.168.51:8090/diariopws/api/1.0/usuario/updateinfsocial";           //en uso
+     public static final String updateMyPublication = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/updatepublicacionpadre";
+     public static final String updateSentimientos = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/updatesentimiento";
+     public static final String updateEvaluacion = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/updateevaluacion";
+     public static final String updateAnalisis = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/updateanalisis";
+     public static final String updateConclusion = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/updateconclusion";
+     public static final String updatePlan = "http://201.144.197.52:8090/diariopws/api/1.0/publicacion/updateplanaccion";
 
+     //URLs Grupo
+     public static final String listgrupo = "http://201.144.197.52:8090/diariopws/api/1.0/grupo/list";            //Funcional en server
+     public static final String listgpoxid = "http://201.144.197.52:8090/diariopws/api/1.0/grupo/listgpoxid";     //Funcional en server " requiere parametro idgrupo"
+     public static final String listuserxgpo = "http://201.144.197.52:8090/diariopws/api/1.0/grupodetalle/listusersgpo";
+     public static final String creategpo = "http://201.144.197.52:8090/diariopws/api/1.0/grupo/create";    //correcion
+     public static final String listargpoxpublicacion = "http://201.144.197.52:8090/diariopws/api/1.0/grupo/listgpoXppadre";
 
-    //URLs listar  publicaciones
-    //  public static final String listarpublicacionxiduser= "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/listxiduser";
-    public static final String listxgrupo = " http://187.188.168.51:8090/diariopws/api/1.0/publicacion/listpublicxgpo";
-    public static final String listpublicacion = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/list";                //en uso
-    public static final String listxiduser = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/listxiduser";  //funcional server "Requiere idusuario" como header
+     //URLs Grupodetalle
+     public static final String listgpodetalle = "http://201.144.197.52:8090/diariopws/api/1.0/grupodetalle/list";   //Funcional en server
+     public static final String addparticipante = "http://201.144.197.52:8090/diariopws/api/1.0/grupodetalle/addparticipante";   //Funcional en server
 
-    //URLs insertar  publicaciones
-    public static final String insertpublicacion = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/createpublicacionpadre";   //En reconstruccion
-    public static final String repuplication = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/republication";
-    public static final String listarrepublication = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/listrepublication";
-    public static final String publicararchivo = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/publicarArchivo";
-    public static final String obtenerdetallepublicacion = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/listdetpublication";
-
-    public static final String updateMyPublication = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/updatepublicacionpadre";
-    public static final String updateSentimientos = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/updatesentimiento";
-    public static final String updateEvaluacion = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/updateevaluacion";
-    public static final String updateAnalisis = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/updateanalisis";
-    public static final String updateConclusion = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/updateconclusion";
-    public static final String updatePlan = "http://187.188.168.51:8090/diariopws/api/1.0/publicacion/updateplanaccion";
-
-    //URLs Grupo
-    public static final String listgrupo = "http://187.188.168.51:8090/diariopws/api/1.0/grupo/list";            //Funcional en server
-    public static final String listgpoxid = "http://187.188.168.51:8090/diariopws/api/1.0/grupo/listgpoxid";     //Funcional en server " requiere parametro idgrupo"
-    public static final String listuserxgpo = "http://187.188.168.51:8090/diariopws/api/1.0/grupodetalle/listusersgpo";
-    public static final String creategpo = "http://187.188.168.51:8090/diariopws/api/1.0/grupo/create";    //correcion
-    public static final String listargpoxpublicacion = "http://187.188.168.51:8090/diariopws/api/1.0/grupo/listgpoXppadre";
-
-    //URLs Grupodetalle
-    public static final String listgpodetalle = "http://187.188.168.51:8090/diariopws/api/1.0/grupodetalle/list";   //Funcional en server
-    public static final String addparticipante = "http://187.188.168.51:8090/diariopws/api/1.0/grupodetalle/addparticipante";   //Funcional en server
-**/
 
     /**   LOCAL       **/
 
 
     /**
      *
-     **/
+
  //URL usuario login
  public static final String login =  "http://192.168.0.104:8080/diariopws/api/1.0/user/autenticar";                        //en uso
  public static final String createaccount =  "http://192.168.0.104:8080/diariopws/api/1.0/usuario/insert";                //en uso
@@ -116,6 +116,6 @@ public class Urls {
  //URLs Grupodetalle
  public static final String listgpodetalle ="http://192.168.0.104:8080/diariopws/api/1.0/grupodetalle/list";   //Funcional en server
  public static final String addparticipante ="http://192.168.0.104:8080/diariopws/api/1.0/grupodetalle/addparticipante";   //Funcional en server
-
+     **/
 
 }
